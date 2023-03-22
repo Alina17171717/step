8a7bb6d8a5ce6fb0476ae07351c72fc5fb27ea70
Alina <alinagvrn@gmail.com>
2023-03-22 23:01:01 +0200

    todo1

---
+++ Dz_9_10_Bilychenko.py
@@ -0,0 +1,14 @@
+from selene import have
+from selene.support.shared import browser
+
+
+def test_complete_todo():
+    browser.open('https://todomvc.com/examples/emberjs/')
+    browser.element('#new-todo').set_value('a').press_enter()
+    browser.element('#new-todo').set_value('b').press_enter()
+    browser.element('#new-todo').set_value('c').press_enter()
+
+    browser.all('#todo-list li').should(have.exact_texts('a', 'b', 'c'))
+    browser.all('#todo-list li .toggle')[1].click()
+    browser.element('#todo-list li.completed').should(have.exact_text('b'))
+    browser.all('#todo-list li:not(.completed)').should(have.exact_texts('a', 'c'))
